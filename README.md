684059720030b43df2e603493fd9c69939eefdd1
Long Song <Larry.Song@civica.com.au>
2024-01-19 11:25:25 +1100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # pdfsize
+
+A recent issue needs to find out system generated pdf page sizes.
+
+PDF pts convert to mm/inchs
+`https://stackoverflow.com/questions/34545339/the-size-of-pdf-documents-how-do-i-convert-from-millimeters-to-pixels-using-spi`
+
+```powershell
+> # load the class
+> . ".\pdfsize.class.ps1"
+>
+> # load from pdf file
+> $pdfinfo = [PdfInfo]::new(".\test.pdf")
+> $pdfinfo
+
+PdfFile                                        Pages
+-------                                        -----
+C:\Users\xxx\Downloads\pdfsize\test.pdf {@{Width=210.016; Height=14.788}, @{Width=210.016; Height=14.788}}
+> # page count
+> $pdfinfo.Pages.Count
+2
+
+> # load from pdf binary
+> [byte[]]$pdfbytes = Get-Content .\test.pdf -Encoding Byte
+> $pdfinfo = [PdfInfo]::new($pdfbytes)
+> $pdfinfo
+
+PdfFile Pages
+------- -----
+        {@{Width=210.016; Height=14.788}, @{Width=210.016; Height=14.788}}
+```
---
+++ pdfsize.class.ps1
@@ -0,0 +1,31 @@
+
+class PdfInfo {
+    $PdfFile
+    $Pages = (New-Object System.Collections.ArrayList)
+
+    PdfInfo([string]$PdfPath) {
+        $this.PdfFile = Get-Item -Path $PdfPath
+        $this.GetPdfPageSizes($this.GetPdfBytes($this.PdfFile.FullName))
+    }
+
+    PdfInfo([byte[]]$PdfBytes) {
+        $this.GetPdfPageSizes($PdfBytes)
+    }
+
+    hidden GetPdfPageSizes([byte[]]$PdfBytes) {
+        $PageSizesMatches = [regex]::Matches(([system.text.encoding]::UTF8.GetString($PdfBytes)), "MediaBox *\[[\d.]+? +?[\d.]+? +?(.+?) .+?(.+?)\]")
+        for($i = 0; $i -lt $PageSizesMatches.Count; $i++)
+        {
+            $this.Pages.Add((
+                New-Object PSObject -Property ([ordered]@{
+                    Width = [math]::Round([decimal]$PageSizesMatches[$i].Groups[1].Value/72*25.4, 3)
+                    Height  = [math]::Round([decimal]$PageSizesMatches[$i].Groups[2].Value/72*25.4, 3)
+                })
+            )) | Out-Null
+        }
+    }
+
+    hidden [byte[]] GetPdfBytes([string]$PdfPath) {
+        return [io.file]::ReadAllBytes($PdfPath)
+    }
+}
